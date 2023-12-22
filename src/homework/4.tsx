470c40be03f5bdd4bb7e7a38efbde7c38c85e55d
Bohdan99I <bohdan.voiyov@gmail.com>
2023-12-22 19:18:16 +0200

    Fix_#4

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -20,9 +20,7 @@ const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
-type PropsProvider = {
-  children: ReactNode; 
-};
+type PropsProvider = { children: ReactNode; };
 
 function MenuProvider({ children }: PropsProvider) { 
   const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({} as SelectedMenu);
@@ -50,9 +48,7 @@ function MenuProvider({ children }: PropsProvider) {
   );
 }
 
-type PropsMenu = {
-  menus: Menu[];
-};
+type PropsMenu = { menus: Menu[]; };
 
 function MenuComponent({ menus }: PropsMenu) {
   const { onSelectedMenu } = useContext(MenuActionContext);
@@ -62,8 +58,7 @@ function MenuComponent({ menus }: PropsMenu) {
     <>
       {menus.map((menu) => (
         <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
+          {menu.title} {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
         </div>
       ))}
     </>
